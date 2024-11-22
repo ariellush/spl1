312f3feb189704369eec23fa3decce3cd5c6ccd4
Ron <ron.sh.10.01@gmail.com>
2024-11-22 14:08:20 +0200

    added constructor, getters and tostring to Settlement.cpp

--- src/Settlement.cpp
+++ src/Settlement.cpp
@@ -1,8 +1,32 @@
 #include "Settlement.h"
 
+//Simple constructor
 Settlement::Settlement(const string &newName,SettlementType newType)
 :name(newName)
 ,type(newType)
 {};
 
+//name getter
 const string &Settlement::getName() const{return name;};
+
+//type getter
+SettlementType Settlement::getType() const{return type;};
+
+
+const string Settlement::toString() const
+{
+    string typeStr;
+    switch (type)
+    {
+        case SettlementType::VILLAGE: typeStr = "Village";
+        break;
+        case SettlementType::CITY: typeStr ="City";
+        break;
+        case SettlementType::METROPOLIS: typeStr = "Metropolis";
+        break;
+        default: typeStr = "undefined";
+        break;
+    }
+
+    return "Settlement - "+name+" | type: "+typeStr;
+};
