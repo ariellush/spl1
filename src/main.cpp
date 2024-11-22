3ee8544936ecc5a86b2ca6d73a05c42eea640b55
Ron <ron.sh.10.01@gmail.com>
2024-11-22 13:40:37 +0200

    upload all files to git

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "/workspaces/project1/include/Simulation.h"
+#include <iostream>
+#include "/workspaces/project1/include/Settlement.h"
+
+using namespace std;
+
+Simulation* backup = nullptr;
+
+int main(int argc, char** argv){
+    if(argc!=2){
+        cout << "usage: simulation <config_path>" << endl;
+        return 0;
+    }
+    string configurationFile = argv[1];
+    Simulation simulation(configurationFile);
+    simulation.start();
+    if(backup!=nullptr){
+    	delete backup;
+    	backup = nullptr;
+    }
+    return 0;
+}
