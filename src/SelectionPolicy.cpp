c3a968584154a2083912132f7932c640d0d89ecb
ariel hartman <arielhr055@gmail.com>
2024-12-05 16:47:22 +0000

    selection

--- src/SelectionPolicy.cpp
+++ src/SelectionPolicy.cpp
@@ -3,12 +3,10 @@
 NaiveSelection :: NaiveSelection() : lastSelectedIndex(-1){}
 const FacilityType& NaiveSelection :: selectFacility(const vector<FacilityType>& facilitiesOptions){
     int currIndex = lastSelectedIndex + 1;
-    FacilityType *toReturn = nullptr;
     if(facilitiesOptions.size() > currIndex){
-        toReturn = new FacilityType(std::move(facilitiesOptions.at(currIndex)));
         lastSelectedIndex = currIndex;
     }
-    return *toReturn;
+    return facilitiesOptions.at(currIndex);
 }
 
 const string NaiveSelection :: toString() const{
@@ -24,32 +22,27 @@ NaiveSelection * NaiveSelection :: clone() const{
 BalancedSelection :: BalancedSelection(int LifeQualityScore, int EconomyScore, int EnvironmentScore) : LifeQualityScore(LifeQualityScore),
 EconomyScore(EconomyScore), EnvironmentScore(EnvironmentScore){};
 
-int BalancedSelection :: getMaxDistance(FacilityType & facility){
+int BalancedSelection :: getMaxDistance(const FacilityType & facility){
     int maxDis = std::max(std::max(facility.getEconomyScore() + EconomyScore, facility.getEnvironmentScore() + EnvironmentScore), facility.getLifeQualityScore() + LifeQualityScore);
     int minDis = std::min(std::min(facility.getEconomyScore() + EconomyScore, facility.getEnvironmentScore() + EnvironmentScore), facility.getLifeQualityScore() + LifeQualityScore); 
     return maxDis - minDis;
 }
 
 const FacilityType& BalancedSelection :: selectFacility(const vector<FacilityType>& facilitiesOptions){
-    FacilityType *toReturn = nullptr;
-    if(facilitiesOptions.size() > 0){
-        toReturn = new FacilityType(std::move(facilitiesOptions.at(0))); 
-        int maxDistanceInmin = getMaxDistance(*toReturn);
-        for(FacilityType currFacility : facilitiesOptions){
-            int currDistance = getMaxDistance(currFacility);
-            if(currDistance < maxDistanceInmin){
-                if(toReturn != nullptr)
-                    delete(toReturn);
-                toReturn = new FacilityType(std::move(currFacility));
-                maxDistanceInmin = currDistance;
-            }
+    int index = 0;
+    int maxDistanceInmin = getMaxDistance(facilitiesOptions.at(0));
+    for(int i = 1; i < facilitiesOptions.size(); i++){
+        int currDistance = getMaxDistance(facilitiesOptions.at(i));
+        if(currDistance < maxDistanceInmin){
+            index = i;
+            maxDistanceInmin = currDistance;
         }
-        //possibly redundant
-        EconomyScore += toReturn->getEconomyScore();
-        EnvironmentScore += toReturn->getEnvironmentScore();
-        LifeQualityScore += toReturn->getLifeQualityScore();
     }
-    return *toReturn;
+    //possibly redundant
+    EconomyScore += facilitiesOptions.at(index).getEconomyScore();
+    EnvironmentScore += facilitiesOptions.at(index).getEnvironmentScore();
+    LifeQualityScore += facilitiesOptions.at(index).getLifeQualityScore();
+    return facilitiesOptions.at(index);
 }    
 
 const string BalancedSelection :: toString() const{
@@ -63,18 +56,17 @@ BalancedSelection * BalancedSelection :: clone() const{
 EconomySelection :: EconomySelection() : lastSelectedIndex(-1){}
 const FacilityType& EconomySelection :: selectFacility(const vector<FacilityType>& facilitiesOptions){
     int currIndex = lastSelectedIndex + 1;
-    FacilityType *toReturn = nullptr;
     if(facilitiesOptions.size() > currIndex){
         bool ecoFound = false;
         for(;currIndex < facilitiesOptions.size() & !ecoFound; currIndex++){
             if(facilitiesOptions.at(currIndex).getCategory() == FacilityCategory :: ECONOMY){
                 ecoFound = true;
-                toReturn = new FacilityType(std::move(facilitiesOptions.at(currIndex))); 
+                currIndex--;
             }
         }
     }
     lastSelectedIndex = currIndex;
-    return *toReturn;
+    return facilitiesOptions.at(currIndex);
 }
 
 const string EconomySelection :: toString() const{
@@ -90,18 +82,17 @@ EconomySelection * EconomySelection :: clone() const{
 SustainabilitySelection :: SustainabilitySelection() : lastSelectedIndex(-1){}
 const FacilityType& SustainabilitySelection :: selectFacility(const vector<FacilityType>& facilitiesOptions){
     int currIndex = lastSelectedIndex + 1;
-    FacilityType *toReturn = nullptr;
     if(facilitiesOptions.size() > currIndex){
         bool lifeFound = false;
         for(;currIndex < facilitiesOptions.size() & !lifeFound; currIndex++){
             if(facilitiesOptions.at(currIndex).getCategory() == FacilityCategory :: LIFE_QUALITY){
                 lifeFound = true;
-                toReturn = new FacilityType(std::move(facilitiesOptions.at(currIndex))); 
+                currIndex--;
             }
         }
     }
     lastSelectedIndex = currIndex;
-    return *toReturn;
+    return facilitiesOptions.at(currIndex);
 }
 
 const string SustainabilitySelection :: toString() const{
